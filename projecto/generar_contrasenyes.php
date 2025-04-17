e66b37ee462e5ebab55ac7ecc7961cccbbcc0cb0
AlexTorresGomez <106146643+AlexTorresGomez@users.noreply.github.com>
2025-04-17 13:55:32 +0200

    corregido i bien calsificado

--- projecto/generar_contrasenyes.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Generar las contraseñas hasheadas
-echo "Admin: " . password_hash("admin123", PASSWORD_DEFAULT) . "\n";
-echo "Usuario: " . password_hash("usuario123", PASSWORD_DEFAULT);
-?>
